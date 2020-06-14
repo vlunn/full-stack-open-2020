68d72011d7c1444b963661a3579bca3eb60efee8
Vivian Lunnikivi <vivian.lunnikivi@tuni.fi>
2020-06-14 17:11:58 +0300

    Exercise 1.7: Unicafe step 2: Add statistics.

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -18,16 +18,26 @@ const Feedback = (props) => {
   )
 }
 
-const StatLine = ({text, score}) =>
-  <div>{text} {score}</div>
+const StatLine = ({text, score, isPercentage}) => {
+  if (isPercentage) {
+    return <div>{text} {score} %</div>
+  }
+  return <div>{text} {score}</div>
+}
 
 const Statistics = (props) => {
+
+  const percentagePositive = (props.scoreAll === 0) ? 0 : props.scorePositive / props.scoreAll * 100
+
   return (
     <div>
       <Title text={'statistics'}/>
-      <StatLine text={props.titleGood} score={props.scoreGood}/>
-      <StatLine text={props.titleNeutral} score={props.scoreNeutral}/>
-      <StatLine text={props.titleBad} score={props.scoreBad}/>
+      <StatLine text={props.titleGood} score={props.scoreGood} isPercentage={false} />
+      <StatLine text={props.titleNeutral} score={props.scoreNeutral} isPercentage={false} />
+      <StatLine text={props.titleBad} score={props.scoreBad} isPercentage={false} />
+      <StatLine text={props.titleAll} score={props.scoreAll} isPercentage={false} />
+      <StatLine text={props.titleAvg} score={props.scoreAvg} isPercentage={false} />
+      <StatLine text={props.titlePositive} score={percentagePositive} isPercentage={true} />
     </div>
   )
 }
@@ -37,20 +47,50 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const titleGood = 'good'
   const titleNeutral = 'neutral'
   const titleBad = 'bad'
+  const titleAll = 'all'
+  const titleAvg = 'average'
+  const titlePositive = 'positive'
+
+  const updateStats = (vote) => {
+    setAll(all + 1)
+    setAvg((good - bad + vote) / (all + 1))
+  }
+
+  const handleGood = () => {
+    setPositive(positive + 1)
+    setGood(good + 1)
+    updateStats(1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    updateStats(0)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+    updateStats(-1)
+  }
 
   return (
     <div>
-      <Feedback handleGood={() => setGood(good + 1)} textGood={titleGood}
-                handleNeutral={() => setNeutral(neutral + 1)} textNeutral={titleNeutral}
-                handleBad={() => setBad(bad + 1)} textBad={titleBad}
+      <Feedback handleGood={handleGood} textGood={titleGood}
+                handleNeutral={handleNeutral} textNeutral={titleNeutral}
+                handleBad={handleBad} textBad={titleBad}
       />
       <Statistics scoreGood={good} titleGood={titleGood} 
                   scoreNeutral={neutral} titleNeutral={titleNeutral} 
                   scoreBad={bad} titleBad={titleBad}
+                  scoreAll={all} titleAll={titleAll}
+                  scoreAvg={avg} titleAvg={titleAvg}
+                  scorePositive={positive} titlePositive={titlePositive}
       />
     </div>
   )
